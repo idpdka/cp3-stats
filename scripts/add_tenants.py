a52a218b8b6163028ab1eb0ef71923b090d8fc1a
Deny Krisna <idpdka@yahoo.com>
2022-06-19 17:50:24 +0700

    modified add_tenants and add_subcomplex

--- scripts/add_tenants.py
+++ scripts/add_tenants.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
 
-engine = create_engine('postgresql://nedkrs@localhost/cp3_stats', echo=False)
+DATABASE_URL = os.environ['DATABASE_URL']
+engine = create_engine(DATABASE_URL, echo=False)
 
 if __name__ == "__main__":
     df = pd.read_csv('./data/cp_tenants.csv', header=0, delimiter=';')
