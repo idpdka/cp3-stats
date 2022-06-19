4ae3599900f69de44e9ff20e471e610b8fbb22be
Deny Krisna <idpdka@yahoo.com>
2022-06-19 18:44:27 +0700

    modified conn config

--- scripts/add_subcomplex.py
+++ scripts/add_subcomplex.py
@@ -1,16 +1,24 @@
-import psycopg2
+import argparse
 import pandas as pd
 import numpy as np
+import os
 from sqlalchemy import create_engine
 
-DATABASE_URL = os.environ['DATABASE_URL']
-engine = create_engine(DATABASE_URL, echo=False)
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ssl", help="Use SSL Mode", action="store_true")
+    args = parser.parse_args()
+
+    DATABASE_URL = os.environ['DATABASE_URL']
+    if args.ssl:
+        conn = create_engine(DATABASE_URL, echo=False)
+    else:
+        conn = create_engine(DATABASE_URL, echo=False)
+
     df = pd.read_csv('./data/cp_subcomplex.csv', header=0, delimiter=';')
     df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
     df['created_at'] = df['created_at'].astype('datetime64[ns]')
     df['updated_at'] = df['updated_at'].astype('datetime64[ns]')
     df['deleted_at'] = df['deleted_at'].astype('datetime64[ns]')
 
-    df.to_sql('subcomplex', if_exists='replace', con=engine, index='id')
+    df.to_sql('subcomplex', if_exists='replace', con=conn, index='id')
--- scripts/add_tenants.py
+++ scripts/add_tenants.py
@@ -1,12 +1,21 @@
+import argparse
 import psycopg2
 import pandas as pd
 import numpy as np
+import os
 from sqlalchemy import create_engine
 
-DATABASE_URL = os.environ['DATABASE_URL']
-engine = create_engine(DATABASE_URL, echo=False)
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ssl", help="Use SSL Mode", action="store_true")
+    args = parser.parse_args()
+
+    DATABASE_URL = os.environ['DATABASE_URL']
+    if args.ssl:
+        conn = create_engine(DATABASE_URL, echo=False)
+    else:
+        conn = create_engine(DATABASE_URL, echo=False)
+
     df = pd.read_csv('./data/cp_tenants.csv', header=0, delimiter=';')
     df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
     df['created_at'] = '2022-06-19 00:00:00'
@@ -17,4 +26,4 @@ if __name__ == "__main__":
     df['deleted_at'] = df['deleted_at'].astype('datetime64[ns]')
     df['funding_confirmed_at'] = df['funding_confirmed_at'].astype('datetime64[ns]')
 
-    df.to_sql('unit', if_exists='replace', con=engine, index='id')
+    df.to_sql('unit', if_exists='replace', con=conn, index='id')
